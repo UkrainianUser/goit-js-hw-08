d2e5c23c59bb57a0760475f6b78a02cdffb509c1
Serhii Ostapenko <ostapkinpaskal@gmail.com>
2023-02-12 17:01:02 +0200

    adds solution task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,23 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+
+const createGalleryItems = items => {
+   const itemEl = galleryItems
+   .map(item => `<li>
+   <a class = gallery__item href = ${item.original}>
+   <img class = gallery__image src = ${item.preview} alt = ${item.description} title = ${item.description}> 
+   </a></li>`)
+   .join('');
+   return itemEl;
+};
+
+galleryEl.insertAdjacentHTML('beforeend', createGalleryItems(galleryItems));
+
+let gallery = new SimpleLightbox('.gallery__item', {captionDelay:  250});
